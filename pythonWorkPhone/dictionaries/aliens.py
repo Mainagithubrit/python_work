53eee2b3cf3ae90a31842f3468ce61ef5a7bab8b
Mainagithubrit <njorogefrancismaina@gmail.com>
2024-09-02 14:38:47 +0300

    pythonWorkPhone

---
+++ pythonWorkPhone/dictionaries/aliens.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""In this program we have a group of dictionaries"""
+
+aliens_0 = {'colors': 'green', 'points': 5}
+aliens_1 = {'colors': 'yellow', 'points': 10}
+aliens_2 = {'colors': 'red', 'points': 15}
+
+aliens = [aliens_0, aliens_1, aliens_2]
+for alien in aliens:
+    print(alien)
