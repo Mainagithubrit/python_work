4266a2e63da2a52762cb51b04143b2105cf2be92
Mainagithubrit <njorogefrancismaina@gmail.com>
2026-05-28 20:51:37 +0300

    understanding data visualization

---
+++ python_work/projects/Data_visualization/mpl_squares.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+input_values = range(1, 1001)
+squares = [x**2 for x in input_values]
+
+plt.style.use('seaborn-v0_8-darkgrid')
+fig, ax = plt.subplots()
+ax.scatter(input_values, squares, c=squares, cmap=plt.cm.Blues, s=10)
+ax.plot(input_values, squares, linewidth=3)
+
+# Set chart title and label axes.
+ax.set_title('Square Numbers', fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+
+ax.axis([0, 1100, 0, 1100000])
+
+# Set size of tick labels.
+ax.tick_params(axis='both', which='major', labelsize=14)
+
+plt.show()
