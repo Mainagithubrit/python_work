53ce634c8f74dc0bc8f558f3a36b948a9804170e
Mainagithubrit <njorogefrancismaina@gmail.com>
2026-06-15 20:22:14 +0300

    Skipping to Django

--- python_work/projects/API/python_repos.py
+++ python_work/projects/API/python_repos.py
@@ -6,8 +6,31 @@ headers = {'Accept': 'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
 print(f'Status code: {r.status_code}')
 
-# Store API response in a variable.
+# Convert the response object to a dictionary
 response_dict = r.json()
+
+print(f"Total repositories: {response_dict['total_count']}")
+print(f'Complete results: {not response_dict['incomplete_results']}')
+
+# Explore information about the repositories
+repo_dicts = response_dict['items']
+print(f'Repositories returned: {len(repo_dicts)}')
+
+# Examine the first repositories
+# repo_dict = repo_dicts[0]
+print("\nSelected information about first repository:")
+for repo_dict in repo_dicts:
+    print(f"Name: {repo_dict['name']}")
+    print(f"Owner: {repo_dict['owner']['login']}")
+    print(f"Stars: {repo_dict['stargazers_count']}")
+    print(f"Repository: {repo_dict['html_url']}")
+    print(f"Created: {repo_dict['created_at']}")
+    print(f"Updated: {repo_dict['updated_at']}")
+    print(f"Description: {repo_dict['description']}")
+    print(f"\nKeys: {len(repo_dict)}")
+
+# for key in sorted(repo_dict.keys()):
+#    print(key)
 
 # Process requests
 print(response_dict.keys())
